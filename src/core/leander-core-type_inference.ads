aa9cd45bcca5602311d515c0aeb7a8e33fc9cd03
Fraser Wilson <blancolioni@gmail.com>
2017-01-01 11:31:48 +0100

    Fixed local/global environment usage

--- src/core/leander-core-type_inference.ads
+++ src/core/leander-core-type_inference.ads
@@ -5,9 +5,6 @@ with Leander.Environments;
 package Leander.Core.Type_Inference is
 
    procedure Infer_Types
-     (Bindings : Leander.Core.Bindings.Binding_List;
-      Types    : Leander.Types.Bindings.Type_Binding_List;
-      Cons     : Leander.Types.Bindings.Constructor_Binding_List;
-      Env      : Leander.Environments.Environment);
+     (Env      : Leander.Environments.Environment);
 
 end Leander.Core.Type_Inference;
