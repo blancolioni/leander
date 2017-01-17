c6a87efafcb9d83bddacac5f52da02c6ae1a89f9
Fraser Wilson <blancolioni@gmail.com>
2017-01-17 15:44:42 +0100

    Added support for derived instances.  Added simple version of Eq derivation

---
+++ src/types/leander-types-instances-derived.ads
@@ -0,0 +1,10 @@
+with Leander.Environments;
+
+package Leander.Types.Instances.Derived is
+
+   procedure Derive_Instance
+     (Env        : Leander.Environments.Environment;
+      Tree       : Leander.Types.Trees.Tree_Type;
+      Class_Name : String);
+
+end Leander.Types.Instances.Derived;
