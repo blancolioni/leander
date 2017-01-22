da8321215431c93e1a9a4bcf2909ad29b5314f30
Fraser Wilson <blancolioni@gmail.com>
2017-01-22 11:54:37 +0100

    Added infix declarations, class Enum, [a] Show instance

--- config/libraries/Prelude.hs
+++ config/libraries/Prelude.hs
@@ -1,44 +1,31 @@
 systemName = "Leander"
 
+foreign import #putchar :: Char -> World# -> World#
+
+infixr 9  .  
+infixr 8  ^, ^^, **
+infixl 7  *, /, `quot`, `rem`, `div`, `mod`
+infixl 6  +, -
+
+-- The (:) operator is built-in syntax, and cannot legally be given  
+-- a fixity declaration; but its fixity is given by:  
+--   infixr 5  :  
+ 
+infix  4  ==, /=, <, <=, >=, >  
+infixr 3  &&  
+infixr 2  ||  
+infixl 1  >>, >>=  
+infixr 1  =<<  
+infixr 0  $, $!, `seq`
+
+
 fix f = f (fix f)
 
--- Map and append  
---  map :: (a -> b) -> [a] -> [b]  
-map f []     = []  
-map f (x:xs) = f x : map f xs
-
-[]     ++ ys = ys  
-(x:xs) ++ ys = x : (xs ++ ys)
-
-filter p []                 = []  
-filter p (x:xs) = if p x then x : filter p xs else filter p xs
-
-concat xss = foldr (++) [] xss
-
-concatMap f = concat . map f
-
--- foldl, applied to a binary operator, a starting value (typically the  
--- left-identity of the operator), and a list, reduces the list using  
--- the binary operator, from left to right:  
---  foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn  
--- foldl1 is a variant that has no starting value argument, and  thus must  
--- be applied to non-empty lists.  scanl is similar to foldl, but returns  
--- a list of successive reduced values from the left:  
---      scanl f z [x1, x2, ...] == [z, z ‘f‘ x1, (z ‘f‘ x1) ‘f‘ x2, ...]  
--- Note that  last (scanl f z xs) == foldl f z xs.  
--- scanl1 is similar, again without the starting element:  
---      scanl1 f [x1, x2, ...] == [x1, x1 ‘f‘ x2, ...]  
+-- Standard types, classes, instances and related functions  
  
-foldl f z []     =  z  
-foldl f z (x:xs) =  foldl f (f z x) xs
-
--- foldr, foldr1, scanr, and scanr1 are the right-to-left duals of the  
--- above functions.  
+-- Equality and Ordered classes  
  
-foldr f z []     =  z  
-foldr f z (x:xs) =  f x (foldr f z xs)
-
-class  Eq a  where  
+ class  Eq a  where  
     (==), (/=) :: a -> a -> Bool  
  
         -- Minimal complete definition:  
@@ -50,10 +37,50 @@ class  (Eq a) => Ord a  where
     compare              :: a -> a -> Ordering  
     (<), (<=), (>=), (>) :: a -> a -> Bool  
     max, min             :: a -> a -> a  
-    
-	max x y = if x <= y then y else x
-	min x y = if x <= y then x else y
+ 
+        -- Minimal complete definition:  
+        --      (<=) or compare  
+        -- Using compare can be more efficient for complex types.  
+    compare x y  = if x == y then EQ
+                   else if X <= y then LT
+                   else GT
+ 
+    x <= y           =  compare x y /= GT  
+    x <  y           =  compare x y == LT  
+    x >= y           =  compare x y /= LT  
+    x >  y           =  compare x y == GT
 	
+-- note that (min x y, max x y) = (x,y) or (y,x)  
+    max x y = if x <= y then y else x
+
+    min x y = if x <= y then x else y
+
+-- Enumeration and Bounded classes  
+ 
+class  Enum a  where  
+    succ, pred       :: a -> a  
+    toEnum           :: Int -> a  
+    fromEnum         :: a -> Int  
+    enumFrom         :: a -> [a]             -- [n..]  
+    enumFromThen     :: a -> a -> [a]        -- [n,n'..]  
+    enumFromTo       :: a -> a -> [a]        -- [n..m]  
+    enumFromThenTo   :: a -> a -> a -> [a]   -- [n,n'..m]  
+ 
+        -- Minimal complete definition:  
+        --      toEnum, fromEnum  
+        --  
+        -- NOTE: these default methods only make sense for types  
+        --       that map injectively into Int using fromEnum  
+        --       and toEnum.  
+    succ             =  toEnum . (+1) . fromEnum  
+    pred             =  toEnum . (subtract 1) . fromEnum  
+    enumFrom x       =  map toEnum [fromEnum x ..]  
+    enumFromTo x y   =  map toEnum [fromEnum x .. fromEnum y]  
+    enumFromThen x y =  map toEnum [fromEnum x, fromEnum y ..]  
+    enumFromThenTo x y z =  
+                        map toEnum [fromEnum x, fromEnum y .. fromEnum z]
+                        
+                        
 class  Show a  where  
     showsPrec        :: Int -> a -> [Char] -> [Char]  
     show             :: a -> [Char]
@@ -73,12 +100,30 @@ showl (x:xs) = showChar ',' . shows x . showl xs
                                              
 shows            =  showsPrec 0
 
+showLitChar :: Char -> [Char] -> [Char]
+showLitChar = (:)
+
 showChar         :: Char -> [Char] -> [Char]
 showChar         =  (:)
 
 showString       :: [Char] -> [Char] -> [Char]
 showString       =  (++)
 
+instance  Show Char  where  
+    showsPrec p '\'' = showString "'\\''"  
+    showsPrec p c    = showChar '\'' . showLitChar c . showChar '\''  
+ 
+    showList cs = showChar '"' . showCharList cs  
+                 	   
+-- showCharList :: [Char] -> [Char] -> [Char]
+showCharList [] = showChar '"'
+showCharList (x:xs) = case x of 
+                        '"' -> showString "\\\"" . showCharList xs
+						_   -> showLitChar x . showCharList xs
+						
+instance  (Show a) => Show [a]  where  
+    showsPrec p      = showList
+	
 class  Functor f  where  
     fmap              :: (a -> b) -> f a -> f b
     
@@ -112,7 +157,6 @@ instance Monad IO where
                                (x,w') -> case g x of
                                           (IO h) -> h w')
 	
-foreign import #putchar :: Char -> World# -> World#
 
 putChar :: Char -> IO ()
 putChar ch = IO (\w -> (\w' -> ((),w')) (#putchar ch w))
@@ -125,18 +169,14 @@ putStrLn s = putStr s >> putChar '\n'
 
 print = putStrLn . show
 
-testPrint = print True
+testPrintBoolList = print [True, False] --  [1,2,3]
+testPrintBool = print True
 
-join (IO f) g = IO (\w -> case f w of
-                               (x,w') -> case g x of
-                                          (IO h) -> h w')
-                                          
+testPrint = testPrintBoolList
+
 runIO w (IO f) = case f w of
                    (x,w') -> w'
                    
-testPutChar1 = join putX (\_ -> putChar 'Y')
-testPutChar = putX >> putChar 'Y'
-
 testPutStrLn = doTestPutStrLn "Hello, world"
 
 doTestPutStrLn [] = putChar '\n'
@@ -173,7 +213,7 @@ not True = False
 
 -- Ordering type  
  
-data  Ordering  =  LT | EQ | GT  
+data  Ordering  =  LT | EQ | GT deriving (Eq,Show)
 
 null [] = True
 null x = False
@@ -197,9 +237,39 @@ data  Either a b  =  Left a | Right b
 either f g (Left x) = f x
 either f g (Right y) = g y
                              
-testEqBool x = x == True
+-- Map and append  
+map :: (a -> b) -> [a] -> [b]  
+map f []     = []  
+map f (x:xs) = f x : map f xs
 
-data NewType a = NewType a
+[]     ++ ys = ys  
+(x:xs) ++ ys = x : (xs ++ ys)
 
-runNewType (NewType f) x = NewType (f x)
-testNewType x = runNewType (NewType id) x
+filter p []                 = []  
+filter p (x:xs) = if p x then x : filter p xs else filter p xs
+
+concat xss = foldr (++) [] xss
+
+concatMap f = concat . map f
+
+-- foldl, applied to a binary operator, a starting value (typically the  
+-- left-identity of the operator), and a list, reduces the list using  
+-- the binary operator, from left to right:  
+--  foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn  
+-- foldl1 is a variant that has no starting value argument, and  thus must  
+-- be applied to non-empty lists.  scanl is similar to foldl, but returns  
+-- a list of successive reduced values from the left:  
+--      scanl f z [x1, x2, ...] == [z, z ‘f‘ x1, (z ‘f‘ x1) ‘f‘ x2, ...]  
+-- Note that  last (scanl f z xs) == foldl f z xs.  
+-- scanl1 is similar, again without the starting element:  
+--      scanl1 f [x1, x2, ...] == [x1, x1 ‘f‘ x2, ...]  
+ 
+foldl f z []     =  z  
+foldl f z (x:xs) =  foldl f (f z x) xs
+
+-- foldr, foldr1, scanr, and scanr1 are the right-to-left duals of the  
+-- above functions.  
+ 
+foldr f z []     =  z  
+foldr f z (x:xs) =  f x (foldr f z xs)
+
