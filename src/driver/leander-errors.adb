97e5a84c20ef7867228715cf66d590c3f6dbf268
Fraser Wilson <blancolioni@gmail.com>
2017-01-24 17:25:14 +0100

    Leander.Errors: keep running count of number of errors reported.  Added function Has_Errors.  Added procedure Clear_Errors.

--- src/driver/leander-errors.adb
+++ src/driver/leander-errors.adb
@@ -1,6 +1,18 @@
 with Ada.Text_IO;
+with Leander.Logging;
 
 package body Leander.Errors is
+
+   Error_Count : Natural := 0;
+
+   ------------------
+   -- Clear_Errors --
+   ------------------
+
+   procedure Clear_Errors is
+   begin
+      Error_Count := 0;
+   end Clear_Errors;
 
    -----------
    -- Error --
@@ -14,6 +26,18 @@ package body Leander.Errors is
       Ada.Text_IO.Put_Line
         (Ada.Text_IO.Standard_Error,
          Leander.Source.Show (Source) & ": " & Message);
+      Leander.Logging.Log
+        (Leander.Source.Show (Source) & ": " & Message);
+      Error_Count := Error_Count + 1;
    end Error;
+
+   ----------------
+   -- Has_Errors --
+   ----------------
+
+   function Has_Errors return Boolean is
+   begin
+      return Error_Count > 0;
+   end Has_Errors;
 
 end Leander.Errors;
--- src/driver/leander-errors.ads
+++ src/driver/leander-errors.ads
@@ -2,6 +2,9 @@ with Leander.Source;
 
 package Leander.Errors is
 
+   function Has_Errors return Boolean;
+   procedure Clear_Errors;
+
    procedure Error
      (Source  : Leander.Source.Source_Reference;
       Message : String);
